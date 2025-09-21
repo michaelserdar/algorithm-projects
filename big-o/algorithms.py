e72769e9760fa3506c3e6f5c8b68768f7607e6e9
michaelserdar <122762214+michaelserdar@users.noreply.github.com>
2025-09-21 17:04:19 -0500

    create (n log n) and quadratic functions

--- big-o/algorithms.py
+++ big-o/algorithms.py
@@ -45,4 +45,71 @@ def binary_search_steps(n: int) -> int:
         return steps
 
 
+# --O(n)---------------------------------------------------
+def linear_sum(n: int) -> int:
+    total = 0
+    for i in range(n):
+        total +=i
+    return total
+
+
+# --O(n log n)--------------------------------------------
+def merge_sort(a: List[int]) -> List[int]:
+    """
+    Merge sort algorithm
+    """
+    # Base case: if the list is length 0 or 1 it is already sorted
+    if len(a) <= 1:
+        return a
+
+    # split the list in half
+    mid = len(a) // 2
+    left = merge_sort(a[:mid])   # recursively sort left half
+    right = merge_sort(a[mid:])  # recursively sort right half
+
+    # merge two sorted lists back into one with merge function
+    return _merge(left, right)
+
+
+def _merge(left: list[int], right: list[int]) -> list[int]:
+    """
+    merge two sorted lists into a single list
+    Key step of merge sort
+    """
+    i = j = 0  # pointers for the left and right lists
+    out = []
+
+    # while both lists have elements left to compare
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            out.append(left[i])
+            i += 1
+        else:
+            out.append(right[j])
+            j += 1
+
+    # to reach this point at least one list is sorted
+    # append any remaining elements from the other list
+    out.extend(left[i:])
+    out.extend(right[j:])
+    return out
+
+
+# --O(n^2)---------------------------------------------------------
+def quadratic_pairs(n: int, return_pairs: bool = False):
+    """
+    Count how many (i, j) pairs with i < j exist among n elements.
+    input: int - number of pairs
+    output: count of pairs or list of pairs if return_pairs = True
+    """
+    count = 0
+    pairs = [] if return_pairs else None
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            count += 1
+            if return_pairs:
+                pairs.append((i,j))
+
+    return (count, pairs) if return_pairs else count
 
