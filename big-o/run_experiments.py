976b5479efaaf3a3a24fd350f7f24af9aa92ce5f
michaelserdar <122762214+michaelserdar@users.noreply.github.com>
2025-09-28 08:02:27 -0500

    created constants for size inputs and added function for time call

--- big-o/run_experiments.py
+++ big-o/run_experiments.py
@@ -20,5 +20,20 @@ from algorithms import (
     fib_exp
 )
 
+FAST = os.environ.get("FAST", "") != ""
+SIZES_LINEAR = [10_000, 50_000, 100_000] if not FAST else [5_000, 20_000]
+SIZES_NLOGN = [5_000, 20_000, 50_000] if not FAST else [2_000, 10_000]
+SIZES_quad = [600, 1_000, 1_400] if not FAST else [300, 600]
+SIZES_EXP = [28, 30] if not FAST else [24, 26]
 
 
+def time_call(fn: Callable, *args, repeats: int = 1) -> float:
+    start = time.perf_counter()
+    for _ in range(repeats):
+        fn(*args)
+    end = time.perf_counter()
+    return (end - start) / repeats
+
+
+if __name__ == "__main__":
+    main()
